d88a70868bfa291cc50042ee276ae52ebda7f2c0
lludford <lludford@u.rochester.edu>
2016-11-11 17:37:21 -0500

    lookup + delete for SNAP

--- project4.c
+++ project4.c
@@ -13,8 +13,7 @@ struct TUPLE {
 	char Name[30];
 	char Address[50];
 	char Phone[8];
-	//struct TUPLE next;
-	//struct TUPLELIST next;
+	TUPLELIST next;
 };
 struct TUPLELIST{
 	struct TUPLE* head;
@@ -32,12 +31,12 @@ struct TUPLE* createTUPLE(int StudentId, char Name[], char Address[], char Phone
 	strcpy(x->Address, Address);
 	x->StudentId = StudentId;
 	strcpy(x->Phone, Phone);
-	//x->next = NULL;
+	x->next = NULL;
 	return x;
 }
 void insert(struct TUPLE* tuple){
 	int hash = h(tuple->StudentId);
-	printf("%d\n\n", hash);
+	//printf("%d\n\n", hash);
 	if (HASHTABLE[hash].head == NULL){
 		HASHTABLE[hash].head = tuple;
 	}
@@ -45,7 +44,17 @@ void insert(struct TUPLE* tuple){
 		HASHTABLE[hash].head = tuple;
 	}
 }
-
+void delete(struct TUPLE* tuple){
+	int hash = h(tuple->StudentId);
+	if (HASHTABLE[hash].head != NULL){
+		HASHTABLE[hash].head = NULL;
+	}
+}
+struct TUPLE* lookup(struct TUPLE* tuple);
+struct TUPLE* lookup(struct TUPLE* tuple){//lookup with the tuple
+	int key = h(tuple->StudentId);
+	return HASHTABLE[key].head;
+}
 int main() {
 	struct TUPLE* x = createTUPLE(000, "Lilian", "1414 woodoak", "213555");
 	printf("%s\n", x->Name);
@@ -53,8 +62,15 @@ int main() {
 	printf("%s\n", HASHTABLE[0].head->Phone);
 	struct TUPLE* m = createTUPLE(02, "lil", "7 afewf", "3335");
 	insert(m);
+	struct TUPLE* n = createTUPLE(3, "hay", "93'2", "2423");
+	insert(n);
 	int m_hash = h(m->StudentId);
 	printf("%s\n", HASHTABLE[m_hash].head->Phone);
+	delete(m);
+	insert(m);
+	printf("%p\n", HASHTABLE[m_hash].head);
+	printf("%s\n", lookup(x)->Name);
+
 	return 0;
 
 }
